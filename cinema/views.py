6fa0cd94105fbea01be2d8439f4386dce31f4630
dsokolovskiy <souldead147@gmail.com>
2024-07-29 11:25:01 +0300

    fix solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,9 +1,10 @@
 from rest_framework.decorators import api_view
-from cinema.models import Movie
-from cinema.serializers import MovieSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
+
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
 
 
 @api_view(["GET", "POST"])
